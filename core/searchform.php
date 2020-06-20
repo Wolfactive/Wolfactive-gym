f55e296e3d1cbbf921ddd94fbd0ea2f193aa3efc
DatTH <57146764+hotandat260399@users.noreply.github.com>
2020-06-20 16:03:46 +0700

    update search form

--- core/searchform.php
+++ core/searchform.php
@@ -1,12 +1,11 @@
-<form role="search" method="get" class="search-form" action="http://gym-and-fitness.local/">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(site_url('/')); ?>">
   <label>
-    <span class="screen-reader-text">Search for:</span>
-    <input type="text" class="search-field" placeholder="Search …" value="" name="s">
+    <!-- <span class="screen-reader-text">Search for:</span> -->
+    <input type="text" class="search-field" placeholder="Tìm Kiếm" value="" name="s">
     <input type="hidden" class="search-field" placeholder="Search …" value="1" name="sentence">
     <input type="hidden" class="search-field" placeholder="Search …" value="post" name="post_type">
   </label>
   <button type="submit" class="search-submit" value="Search" aria-label="Button Submit Search">
     <i class="fas fa-search"></i>
-    <p>Tìm Kiếm</p>
   </button>
 </form>
