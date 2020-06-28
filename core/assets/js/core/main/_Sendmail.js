1f7c7af27330a9436ae728e66c279b6858ef607f
DatTH <57146764+hotandat260399@users.noreply.github.com>
2020-06-29 01:02:05 +0700

    update use function to call Post in Home Page, Update Validate

--- core/assets/js/core/main/_Sendmail.js
+++ core/assets/js/core/main/_Sendmail.js
@@ -1,3 +1,5 @@
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
 /*Var*/
 var valueName = document.querySelector('#HovaTen');
 var valueNameVal = document.querySelector('#HovaTenVal');
@@ -33,22 +35,6 @@ function checkName(){
   valueNameVal.style.opacity = "1";
   isName = false;
 };
-function CheckPhoneNumber(){
-  var patternPhone = new RegExp(
-     "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$"
- );
- if(valueSDT.value){
-   if (!valueSDT.test(valueSDT.value)) {
-       valueSDTVal.style.opacity = "1";
-       isPhone = false;
-   }else{
-     isPhone = true;
-   }
- }else{
-   valueSDTVal.style.opacity = "1";
-   isPhone = true;
- }
-};
 function checkEmptyPhone(){
   valueSDTVal.style.opacity = "1";
   isPhone = false;
@@ -57,7 +43,73 @@ function checkEmptyEmail(){
   valueEmailVal.style.opacity = "1";
   isEmail = false;
 };
-
+function CheckPhoneNumber(){
+  var patternPhone = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+ var SDT = document.getElementById('SDT').value;
+ if(SDT){
+   if (!SDT.match(patternPhone)) {
+       valueSDTVal.style.opacity = "1";
+       isPhone = false;
+       console.log(isPhone);
+   }else{
+     isPhone = true;
+     valueSDTVal.style.opacity = "0";
+     console.log(isPhone);
+   }
+ }else{
+   valueSDTVal.style.opacity = "1";
+   isPhone = false;
+ }
+};
+function CheckMailVaildate(){
+  var Email=document.getElementById('Email').value;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(Email.match(mailformat)){
+    isEmail = true;
+  }
+  else{
+    valueEmailVal.style.opacity = "1";
+    isEmail = false;
+  }
+};
+function checkComment(){
+  if(valueNoiDung.value ===''){
+    valueNoiDungVal.style.opacity = "1";
+    isComment = false;
+  }
+  else {
+    valueNoiDungVal.style.opacity = "0";
+    isComment = true;
+  }
+};
+function checkValidate(){
+ valueName.value ? isName = true : checkName();
+ valueSDT.value ? CheckPhoneNumber() : checkEmptyPhone();
+ valueEmail.value ? CheckMailVaildate(): checkEmptyEmail();
+ valueNoiDung.value !== "" ?isComment = true :checkComment();
+ isName === true && isEmail == true && isPhone === true && isComment === true ? isValidate = true : isValidate = false;
+}
+function sendMail(){
+  var Hovaten = document.getElementById('HovaTen').value;
+  var Mail =  document.getElementById('Email').value;
+  var dienthoai =  document.getElementById('SDT').value;
+  var NoiDung = document.getElementById('Noidung').value;
+  Email.send({
+      SecureToken : "62905d79-1ce1-4f21-a94b-2bc1b9eeff93 ",
+      To : "info.wolfactive@gmail.com",
+      From : Mail,
+      Subject : "[THÔNG TIN LIÊN HỆ] - " + Mail +" - "+ dienthoai +" - "+ Hovaten,
+      Body : NoiDung
+  }).then(
+    alert('Gửi thành công')
+  );
+  Redirect();
+}
+function Redirect(){
+  location.reload()
+}
 function checkValue(){
-  console.log(valueName);
+  checkValidate();
+  console.log(isValidate);
+  isValidate === false ? {} :sendMail();
 }
--- core/sections/blog-homepage-1.php
+++ core/sections/blog-homepage-1.php
@@ -15,57 +15,7 @@
            'posts_per_page' => 4,
         );
         $the_query= new WP_Query($args);
-        if($getValueSelectCategory === get_cat_ID('Class')){
-        while($the_query->have_posts()) : $the_query->the_post(); ?>
-           <li class="class__item">
-             <div class="class__item-contain" data-aos="zoom-out-up" data-aos-duration="2000">
-               <div class="class__image">
-                 <a href="<?php echo get_permalink(); ?>"><img data-src="<?php echo hk_get_thumb(get_the_id(),$width,$height) ?>" alt="Image"></a>
-               </div>
-               <div class="class__title-and-time">
-                 <h3 class="class__name text--upcase"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-                 <p class="class__time"><?php echo get_field('time_open_class') ?></p>
-               </div>
-             </div>
-           </li>
-        <?php
-        endwhile;
-      }
-      else{
-        while($the_query->have_posts()) : $the_query->the_post(); ?>
-           <li class="blog__item">
-             <div class="blog__item-contain" data-aos="zoom-out-up" data-aos-duration="2000">
-               <div class="blog__image">
-                 <a href="<?php echo get_permalink(); ?>"><img data-src="<?php echo hk_get_thumb(get_the_id(),$width,$height) ?>" alt="Image"></a>
-               </div>
-                 <div class="blog__tags">
-                   <?php
-                   $tags = get_the_tags();
-                   if ($tags) {
-                     $i=0;
-                     foreach( $tags as $tag ) {
-                       if($i===0) {echo '<span class="tags-item text--upcase"><a href="'.get_permalink().'">'.$tag->name.'</a></span>';}
-                       $i++;
-                     }
-                   }  ?>
-                 </div>
-                 <div class="bg-cover-color"></div>
-                 <div class="blog__info">
-                   <h3 class="blog__name text--upcase"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
-                   <div class="blog__author-date">
-                     <div class="blog__author">
-                       <p class="author__name"><span class="by-date">By: </span><?php the_author(); ?></p>
-                     </div>
-                     <div class="blog__date">
-                       <p class="date__post"><span class="by-date">Date: </span><?php echo get_the_date(); ?></p>
-                     </div>
-                   </div>
-                 </div>
-             </div>
-           </li>
-        <?php
-        endwhile;
-      }
+        get_post_homepage($getValueSelectCategory,$the_query,$width,$height);
         $cat_id = $getValueSelectCategory;
          ?>
       </ul>
